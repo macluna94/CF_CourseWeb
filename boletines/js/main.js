89396183d421e200fd9d82d25e32f97d6d4b8f16
macluna-linux <macluna94@gmail.com>
2019-03-26 23:26:43 -0600

    application js

---
+++ boletines/js/main.js
@@ -0,0 +1,27 @@
+console.log("inicio")
+
+
+let links = document.querySelectorAll(".close");
+
+
+links.forEach(function(link){
+
+	link.addEventListener("click", function(ev){
+		//ev.preventDefault();
+
+		let content = document.querySelector(".content");
+		// Remover
+		content.classList.remove("fadeInDown");
+		content.classList.remove("animated");
+		// Agregar
+		content.classList.add("fadeOutUp");
+		content.classList.add("animated");
+
+		setTimeout(function(){
+			location.href = "/";
+		},1000);
+		return false;
+	})
+
+
+})
